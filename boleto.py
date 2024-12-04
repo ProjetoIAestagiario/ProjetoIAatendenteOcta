f3bbfaa3844d79c1440a35dbb2947bfb674b5836
ProjetoIAestagiario <projetosiaestagio@gmail.com>
2024-12-04 14:43:48 -0300

    04/12/2022

---
+++ boleto.py
@@ -0,0 +1,22 @@
+from bibliotecas.importacoes import *
+from voz.voz_assistente import *
+from assistente import *
+
+
+def imprimir_boleto_cliente():
+
+    while True:
+        falar("Que que eu imprima o seu boleto?")
+        comando = capturar_fala()
+
+        if comando and "sim" in comando.lower():
+            cpf = pedir_cpf()
+            chamar_boleto(cpf)
+        elif comando and "não" in comando.lower():
+            cpf = pedir_cpf()
+            chamar_teste_velocidade(cpf)
+        elif comando and "sair" in comando.lower():
+            falar("Obrigado por utilizar o assistente da Octa. Até logo!")
+            break
+        else:
+            falar("Desculpe, não entendi. Por favor, repita sua solicitação.")
